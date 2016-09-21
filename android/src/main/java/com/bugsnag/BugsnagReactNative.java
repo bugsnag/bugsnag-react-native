c92f1be5f2db09613eb2dd351d276d334acd3bc9
Mike <mike@bugsnag.com>
2016-09-21 11:41:54 +0100

    Handle missing method during stacktrace parsing

--- android/src/main/java/com/bugsnag/BugsnagReactNative.java
+++ android/src/main/java/com/bugsnag/BugsnagReactNative.java
@@ -295,42 +295,41 @@ class JavaScriptException extends Exception implements JsonStream.Streamable {
         for (String rawFrame : rawStacktrace.split("\\n")) {
             writer.beginObject();
             String methodComponents[] = rawFrame.split("@", 2);
+            String fragment = methodComponents[0];
             if (methodComponents.length == 2) {
                 writer.name("method").value(methodComponents[0]);
-
-                String fragment = methodComponents[1];
-                int columnIndex = fragment.lastIndexOf(":");
-                if (columnIndex != -1) {
-                    String columnString = fragment.substring(columnIndex + 1, fragment.length());
-                    try {
-                        int columnNumber = Integer.parseInt(columnString);
-                        writer.name("columnNumber").value(columnNumber);
-                    } catch (NumberFormatException e) {
-                        BugsnagReactNative.logger.info(String.format(
-                                    "Failed to parse column: '%s'",
-                                    columnString));
-                    }
-                    fragment = fragment.substring(0, columnIndex);
-                }
-
-                int lineNumberIndex = fragment.lastIndexOf(":");
-                if (lineNumberIndex != -1) {
-                    String lineNumberString = fragment.substring(lineNumberIndex + 1, fragment.length());
-                    try {
-                        int lineNumber = Integer.parseInt(lineNumberString);
-                        writer.name("lineNumber").value(lineNumber);
-                    } catch (NumberFormatException e) {
-                        BugsnagReactNative.logger.info(String.format(
-                                    "Failed to parse lineNumber: '%s'",
-                                    lineNumberString));
-                    }
-                    fragment = fragment.substring(0, lineNumberIndex);
-                }
-
-                writer.name("file").value(fragment);
-            } else {
-                writer.name("file").value(rawFrame);
+                fragment = methodComponents[1];
             }
+
+            int columnIndex = fragment.lastIndexOf(":");
+            if (columnIndex != -1) {
+                String columnString = fragment.substring(columnIndex + 1, fragment.length());
+                try {
+                    int columnNumber = Integer.parseInt(columnString);
+                    writer.name("columnNumber").value(columnNumber);
+                } catch (NumberFormatException e) {
+                    BugsnagReactNative.logger.info(String.format(
+                                "Failed to parse column: '%s'",
+                                columnString));
+                }
+                fragment = fragment.substring(0, columnIndex);
+            }
+
+            int lineNumberIndex = fragment.lastIndexOf(":");
+            if (lineNumberIndex != -1) {
+                String lineNumberString = fragment.substring(lineNumberIndex + 1, fragment.length());
+                try {
+                    int lineNumber = Integer.parseInt(lineNumberString);
+                    writer.name("lineNumber").value(lineNumber);
+                } catch (NumberFormatException e) {
+                    BugsnagReactNative.logger.info(String.format(
+                                "Failed to parse lineNumber: '%s'",
+                                lineNumberString));
+                }
+                fragment = fragment.substring(0, lineNumberIndex);
+            }
+
+            writer.name("file").value(fragment);
             writer.endObject();
         }
         writer.endArray();
