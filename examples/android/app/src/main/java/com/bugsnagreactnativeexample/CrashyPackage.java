09f99d79a6c061fecc15c3410eabca5387bcf358
Jamie Lynch <fractalwrench@gmail.com>
2017-07-19 14:18:03 +0100

    add backward compatibility for crashypackage android class

--- examples/android/app/src/main/java/com/bugsnagreactnativeexample/CrashyPackage.java
+++ examples/android/app/src/main/java/com/bugsnagreactnativeexample/CrashyPackage.java
@@ -12,6 +12,10 @@ import java.util.List;
 
 class CrashyPackage implements ReactPackage {
 
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+    return Collections.emptyList();
+  }
+
   @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
     return Collections.emptyList();
