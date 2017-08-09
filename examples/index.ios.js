12810a4bfb33a492fa04d87001e8c0fa31e7cfff
Delisa Mason <iskanamagus@gmail.com>
2017-08-09 16:58:48 -0700

    Fix example app load failure, per facebook/react-native#12588
    Ref: https://github.com/facebook/react-native/issues/12588

--- examples/index.ios.js
+++ examples/index.ios.js
@@ -4,7 +4,7 @@
  * @flow
  */
 
-import App from './app';
+import App from './app/index';
 import { AppRegistry } from 'react-native';
 
 AppRegistry.registerComponent('BugsnagReactNativeExample', () => App);
