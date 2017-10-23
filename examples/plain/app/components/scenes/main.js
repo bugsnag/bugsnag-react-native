8662e22e5e1bb4420cd1debca41e57a8191fd763
Sabrina Tee <30786906+sabbyt54@users.noreply.github.com>
2017-10-23 15:49:11 +0800

    Update main.js

--- examples/plain/app/components/scenes/main.js
+++ examples/plain/app/components/scenes/main.js
@@ -1,8 +1,9 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Navigator } from 'react-native-deprecated-custom-components';
 import { List } from 'react-native-elements';
 import ListItem from '../list_item';
+import PropTypes from 'prop-types';
 
 const styles = StyleSheet.create({
   container: {
