00d50d418388e1bd35e8684f435e1398458f4169
fractalwrench <fractalwrench@gmail.com>
2018-08-10 14:00:34 +0100

    codepush updates

--- examples/codepush/index.js
+++ examples/codepush/index.js
@@ -1,7 +1,4 @@
-/** @format */
+import App from './app/index';
+import { AppRegistry } from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('codepush', () => App);
--- examples/codepush/ios/codepush/AppDelegate.m
+++ examples/codepush/ios/codepush/AppDelegate.m
@@ -9,11 +9,13 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <BugsnagReactNative/BugsnagReactNative.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [BugsnagReactNative start];
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
